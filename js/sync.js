ea3c71bb7b979a3124b9004abbcdc7864b6b7fed
Tom Torres <300awoo@gmail.com>
2016-06-10 12:09:15 +0800

    completed hiding controls via jQuery

--- js/sync.js
+++ js/sync.js
@@ -2,14 +2,29 @@ $(document).ready(function() {
   console.log("jQuery Ready!");
   
   //Hiding bottom buttons on mouse hover
-  var videoContainer = $("#video-container");
+  var videoContainer = $(".wrapper");
   var controls = $("#bottom-buttons");
+  var progressBar = $("#seek-bar");
+  var wholeControls = $('#control');
   
   videoContainer.mouseleave(function() {
-    controls.fadeOut('400');
+//    controls.toggle("blind");
+    controls.hide();
+    wholeControls.removeClass('controlSection');
+    wholeControls.addClass('progressDown');
   });
   
   videoContainer.mouseenter(function() {
-    controls.fadeIn('400');
+    controls.fadeIn('100');
+    wholeControls.removeClass('progressDown');
+    wholeControls.addClass('controlSection');
   });
+
 });
+
+//
+//
+//  videoContainer.mouseleave(function() {
+//    progressBar.removeClass('controlSection');
+//    controls.fadeOut('100');
+//  });
